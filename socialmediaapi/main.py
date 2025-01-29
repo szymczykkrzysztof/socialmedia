e853118cd4df01bb5dcc23b0934c450168b86b5b
krzysztof <krzysztof.szymczyk@yahoo.com>
2025-01-29 13:55:09 +0100

    posts endpoint completed

--- socialmediaapi/main.py
+++ socialmediaapi/main.py
@@ -1,39 +1,10 @@
+import uvicorn
 from fastapi import FastAPI
-from pydantic import BaseModel
+
+from socialmediaapi.routers.post import router as post_router
 
 app = FastAPI()
+app.include_router(post_router)
 
-
-class UserPostIn(BaseModel):
-    body: str
-
-
-class UserPost(UserPostIn):
-    id: int
-
-
-post_table = {}
-
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
-
-
-@app.get("/post")
-async def post():
-    return post_table.items()
-
-
-@app.post("/post", response_model=UserPost)
-async def create_post(post: UserPostIn):
-    data = post.model_dump()
-    last_record_id = len(post_table)
-    new_record = {**data, "id": last_record_id}
-    post_table[last_record_id] = new_record
-    return new_record
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="0.0.0.0", port=8000)
